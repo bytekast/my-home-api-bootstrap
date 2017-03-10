b4af26d3c6f790c1dcf0823f44abd4c7baf1293a
Rowell Belen <rbelen@tendrilinc.com>
2017-03-10 10:03:30 -0700

    Update package names

--- src/main/java/com/bytekast/hackathon/api/MainController.java
+++ src/main/java/com/bytekast/hackathon/api/MainController.java
@@ -1,4 +1,4 @@
-package com.tendril.myhome.api;
+package com.bytekast.hackathon.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/bytekast/hackathon/api/MainControllerTest.java
+++ src/test/java/com/bytekast/hackathon/api/MainControllerTest.java
@@ -1,4 +1,4 @@
-package com.tendril.myhome.api;
+package com.bytekast.hackathon.api;
 
 
 import org.junit.Test;
